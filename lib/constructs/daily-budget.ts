854c3a362bd486a87c1d8833af07fa21b8066e0f
Ylkael <ylkael@hotmail.com>
2023-04-14 11:12:45 +0300

    daily budget construct

---
+++ lib/constructs/daily-budget.ts
@@ -0,0 +1,41 @@
+import * as budgets from 'aws-cdk-lib/aws-budgets';
+import { Construct } from 'constructs';
+
+interface DailyBudgetProps {
+    DailyBudgetAmount: number,
+    emailaddress: string,
+}
+
+export class DailyBudgets extends Construct {
+  constructor(scope: Construct, id: string, props: DailyBudgetProps) {
+    super(scope, id);
+
+    new budgets.CfnBudget(this, 'DailyBudget', {
+        budget: {
+            budgetLimit: {
+                amount: props.DailyBudgetAmount,
+                unit: 'USD'
+            },
+            budgetName: 'DailyBudget',
+            budgetType: 'COST',
+            timeUnit: 'DAILY'            
+        },
+        notificationsWithSubscribers: [
+            {
+                notification: {
+                    comparisonOperator: 'GREATER_THAN',
+                    notificationType: 'ACTUAL',
+                    threshold: 100,
+                    thresholdType: 'PERCENTAGE',
+                },
+                subscribers: [
+                    {
+                        address: props.emailaddress,
+                        subscriptionType: 'EMAIL',
+                    }, 
+                ],
+            },
+        ],
+    });
+  }
+}
